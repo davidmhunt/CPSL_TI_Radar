12b6dd869b8ab209305ce642c44b2ca8016a8e49
David Hunt <david.hunt@duke.edu>
2023-08-07 11:28:52 -0400

    updating run_radar with cleaned file structure

--- CPSL_TI_Radar/run_radar.py
+++ CPSL_TI_Radar/run_radar.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     parser.add_argument(
         'settings_path',
           type=str, nargs='?',
-            default='/home/david/Documents/TI_Radar_Demo_Visualizer/Python_Modules/CPSL_TI_Radar/CPSL_TI_Radar_settings.json',
+            default='CPSL_TI_Radar_settings.json',
               help='path to settings file')
     args = parser.parse_args()
 
