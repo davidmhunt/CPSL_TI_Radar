2451f1eb9f98692011195af14f375be65b7350c4
David Hunt <david.hunt@duke.edu>
2023-10-25 15:20:51 -0400

    updating tests to evaluate ethernet connection

--- CPSL_TI_Radar/tests/test_serial.py
+++ CPSL_TI_Radar/tests/test_serial.py
@@ -138,3 +138,103 @@ def check_streamer_DCA1000():
     except socket.error:
         init_success = False
     assert init_success, "Failed to connect to cmd socket"
+
+    #send sample bytes
+    SYSTEM_CONNECT = 0x09
+    READ_FPGA_VERSION = 0x0E
+
+    #send FPGA system connect
+    # DCA1000 status returns 0 if successful, 1 if not
+    num_fails = 0
+
+    # send System_connect
+    send_command(cmd_socket,FPGA_IP,cmd_port,SYSTEM_CONNECT)
+    num_fails += recv_command_response(cmd_socket,SYSTEM_CONNECT)
+
+    # # send FPGA RESET Command
+    # send_command(RESET_FPGA)
+    # num_fails += recv_command_response(RESET_FPGA)
+
+    # # CONFIG_PACKET_DATA
+    # send_CONFIG_PACKET_DATA()
+    # num_fails += recv_command_response(CONFIG_PACKET_DATA)
+
+    # # send CONFIG_FPGA_GEN
+    # send_CONFIG_FPGA_GEN()
+    # num_fails += recv_command_response(CONFIG_FPGA_GEN)
+
+    # send READ_FPGA_VERSION
+    send_command(cmd_socket,FPGA_IP,cmd_port,READ_FPGA_VERSION)
+    FPGA_version = recv_command_response(cmd_socket,READ_FPGA_VERSION)
+    decode_FPGA_version(FPGA_version)
+
+    # check init satus
+    assert num_fails == 0, "Failed to initialize DCA1000 FPGA"
+
+    return
+
+def recv_command_response(cmd_socket:socket.socket, command_code: int):
+    """Receive the response from a given command code
+
+    Args:
+        command_code (int): the command code for the command that was sent
+
+    Returns:
+        status: the status of the received command
+    """
+
+    msg, server = cmd_socket.recvfrom(2048)
+
+    header = "".join("{:02x}".format(x) for x in struct.unpack("<H", msg[0:2]))
+    recv_cmd_code = struct.unpack("<H", msg[2:4])[0]
+    status = struct.unpack("<H", msg[4:6])[0]
+    footer = "".join("{:02x}".format(x) for x in struct.unpack("<H", msg[6:8]))
+
+    assert recv_cmd_code == command_code, "expected to receive cmd code: {}, but got {}".format(recv_cmd_code,command_code)
+    
+    return status
+
+def send_command(cmd_socket:socket.socket, FPGA_IP, cmd_port, command_code):
+    """Send command to the DCA1000
+
+    Args:
+        command_code (int): Command Code to send
+        data (bytearray, optional): Data associated with the command. Defaults to None.
+    """
+    cmd_header = 0xA55A
+    cmd_footer = 0xEEAA
+
+    # create the byte array
+    msg = bytearray()
+
+    # add the header (formatted as little-endian)
+    msg.extend(struct.pack("<H", cmd_header))
+
+    # add the message code
+    msg.extend(struct.pack("<H", command_code))
+
+    # add the data length
+    msg.extend(struct.pack("<H", 0))
+
+    # add the footer
+    msg.extend(struct.pack("<H", cmd_footer))
+
+    # send the command
+    cmd_socket.sendto(msg, (FPGA_IP, cmd_port))
+
+def decode_FPGA_version(received_messsage: int):
+        """Decode the FPGA version and save it in the FPGA_version variable
+
+        Args:
+            recieved_message (int): the raw status message received from the DCA1000 decoded as uint16
+        """
+
+        # convert to uint16 in little-endian format
+        byte_data = struct.pack(">H", received_messsage)
+
+        major_version = int(byte_data[1]) & 0b01111111
+        minor_version = int.from_bytes(byte_data, byteorder="big") >> 7
+
+        FPGA_version = "{}.{}".format(major_version, minor_version)
+
+        return FPGA_version
